a4ffa2bde6e5a2c723d993db40a08e883ebdfab0
Jrabbit56 <becontinue17@gmail.com>
2022-01-11 21:50:34 +0700

    Update 2022-01-11

--- app/Controllers/ElectricBill.php
+++ app/Controllers/ElectricBill.php
@@ -2,6 +2,8 @@
 
 namespace App\Controllers;
 
+use App\Models\RoomModel;
+
 class ElectricBill extends BaseController 
 {
     public function index()
@@ -9,13 +11,22 @@ class ElectricBill extends BaseController
         $model = model(BuildingModel::class);
         $buildings = $model->getAllBuildings();
         
-        
+               
         $this->renderView('ElectricBill/index', [
             'buildings' => $buildings
         ]);
-    }
-    public function building($id)
+
+            }
+
+    public function building()
     {
-            echo $id ;
+        $roomModel = model(RoomModel::class);
+        $rooms =$roomModel->getAllRooms();   
+
+        $this->renderView('ElectricBill/index/building', [
+            'rooms' => $rooms
+        ]);
     }
+
+    
 }
--- app/Models/RoomModel.php
+++ app/Models/RoomModel.php
@@ -7,4 +7,7 @@ use CodeIgniter\Model;
 class RoomModel extends Model {
     protected $table = "room";
     protected $primaryKey = "RoomID";
+
+    public function getAllrooms() {
+        return $this->orderBy('RoomID', 'asc')->findAll();
 }
